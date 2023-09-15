fb48f4e09c4b4d527bf9915c2959a0a3c26d1d97
Suman Panigrahi <psuman1406@gmail.com>
2023-09-15 11:07:37 +0530

    Second commit

---
+++ books.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+main().then((res) => { console.log('connection successful'); })
+    .catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
+}
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+    },
+    price: {
+        type: Number,
+    },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+let book1= new Book({
+    title: "Math XII",
+    author: "RD SHarma",
+    price:1200,
+});
+
+book1.save().then(res => {
+    console.log(res);
+}).catch(err => {
+    console.log(err);
+});
